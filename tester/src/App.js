fd87c9e0c55d09c968da3348deeda66ba113e34a
Nolan Allen <nolan.r.allen@gmail.com>
2019-06-18 18:15:25 -0500

    3rd commit: accidentally started the project in reverse, finished building initial App framework

--- tester/src/App.js
+++ tester/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ball {this.props.ball}
+          {" "}
+          Strike {this.props.strike}
+          {" "}
+          {this.props.foul} Foul Balls
+          {" "}
+          {this.props.hits} Hits
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        <p>
+          <button>Pitcher Flubbed It</button>
+          {" "}
+          <button>Batter's Blind</button>
+          {" "}
+          <button>Can He Even Hit The Ball?!</button>
+          {" "}
+          <button>FINALLY!!!!</button>
+        </p>
+      </div>
+    );
+  };
+};
