49afa0a78aff92a5d4247fac8b8fa1816b9151dc
Nolan Allen <nolan.r.allen@gmail.com>
2019-06-19 21:06:35 -0500

    5th commit: finished with Jeremys help

--- tester/src/App.js
+++ tester/src/App.js
@@ -1,41 +1,126 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { ball, strike, foul, hits } from './stores/actions';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Display from "./components/Display";
 
-class App extends React.Component {
+class App extends Component {
+  state = {
+    balls: 0,
+    strikes: 0,
+    hits: 0,
+    outs: 0,
+    inning: 1
+  };
+
   render() {
     return (
-      <div>
-        <div>
-          Ball {this.props.ball}
-          {" "}
-          Strike {this.props.strike}
-          {" "}
-          {this.props.foul} Foul Balls
-          {" "}
-          {this.props.hits} Hits
-        </div>
-        <div>
-          <button>Pitcher Flubbed It</button>
-          {" "}
-          <button>Batter's Blind</button>
-          {" "}
-          <button>Can He Even Hit The Ball?!</button>
-          {" "}
-          <button>FINALLY!!!!</button>
-        </div>
+      <div className="App">
+        <Display
+          balls={this.state.balls}
+          strikes={this.state.strikes}
+          hits={this.state.hits}
+          outs={this.state.outs}
+          innings={this.state.inning}
+        />
+        <Dashboard
+          hit={this.handleHits}
+          strike={this.handleStrikes}
+          ball={this.handleBalls}
+          foul={this.handleFouls}
+          out={this.handleOuts}
+        />
       </div>
     );
   };
-};
 
-const mapPropsToState = (state) => {
-  return {
-    ball: state.ball,
-    strike: state.strike,
-    foul: state.foul,
-    hits: state.hits
+  handleHits = e => {
+    e.preventDefault();
+    this.setState({
+      strikes: 0,
+      balls: 0,
+      hits: this.state.hits + 1
+    });
+  };
+
+  handleBalls = e => {
+    e.preventDefault();
+    if (this.state.balls < 3) {
+      const ballCount = this.state.balls;
+      this.setState({
+        balls: ballCount + 1
+      });
+    } else {
+      const ballCount = 0;
+      this.setState({
+        balls: ballCount,
+        strikes: 0
+      });
+    };
+  };
+
+  handleFouls = e => {
+    e.preventDefault();
+    if (this.state.strikes < 2) {
+      const foulBall = this.state.strikes;
+      this.setState({
+        strikes: foulBall + 1
+      });
+    };
+  };
+
+  handleStrikes = e => {
+    e.preventDefault();
+    if (this.state.strikes < 2) {
+      const strikeCount = this.state.strikes;
+      this.setState({
+        strikes: strikeCount + 1
+      });
+    } else {
+      const strikeCount = 0;
+      this.setState({
+        strikes: strikeCount,
+        balls: 0,
+        outs: function outHandler() {
+          if (this.state.outs < 2) {
+            const outCount = this.state.outs;
+            this.setState({
+              outs: outCount + 1
+            });
+          } else {
+            const outCount = 0;
+            const inningHalf = this.state.inning;
+            this.setState({
+              balls: 0,
+              strikes: 0,
+              hits: 0,
+              outs: outCount,
+              inning: inningHalf + 1
+            });
+          };
+        }
+      });
+    };
+  };
+
+  handleOuts = e => {
+    e.preventDefault();
+    if (this.state.outs < 2) {
+      const outCount = this.state.outs;
+      this.setState({
+        outs: outCount + 1
+      });
+    } else {
+      const outCount = 0;
+      const inningHalf = this.state.inning;
+      this.setState({
+        balls: 0,
+        strikes: 0,
+        hits: 0,
+        outs: outCount,
+        inning: inningHalf + 1
+      });
+    };
   };
 };
 
-export default connect(mapPropsToState, { ball, strike, foul, hits })(App);
+export default App;
--- tester/src/index.js
+++ tester/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
-import counter from './stores/reducers';
+// import counter from './stores/reducers';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(counter);
+// const store = createStore(counter);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  // <Provider store={store}>
+    <App />
+  // </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
